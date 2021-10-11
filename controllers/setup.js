70d719bed288effbf0fd4b1080dcca3741d7ba03
Peter Širka <petersirka@gmail.com>
2021-10-11 13:13:23 +0200

    Added readme/config for extensions.

--- controllers/setup.js
+++ controllers/setup.js
@@ -6,10 +6,13 @@ exports.install = function() {
 	ROUTE('API    @setup    +save                      *Setup        --> save');
 	ROUTE('API    @setup    -read                      *Setup        --> read');
 	ROUTE('API    @setup    -usage                     *Setup        --> consumption');
+	ROUTE('API    @setup    -config_read/{id}          *Config       --> read');
+	ROUTE('API    @setup    +config_save               *Config       --> save');
 	ROUTE('API    @setup    -extensions_list           *Extensions   --> list');
 	ROUTE('API    @setup    +extensions_save           *Extensions   --> save');
-	ROUTE('API    @setup    -extensions_remove/id      *Extensions   --> remove');
-	ROUTE('API    @setup    -extensions_download/id    *Extensions   --> download');
+	ROUTE('API    @setup    -extensions_remove/{id}    *Extensions   --> remove');
+	ROUTE('API    @setup    -extensions_download/{id}  *Extensions   --> download');
+	ROUTE('API    @setup    -extensions_readme/{id}    *Extensions   --> readme');
 
 	ROUTE('+SOCKET /setup/  @setup');
 	ROUTE('+POST   /setup/', update, ['upload'], 1024 * 10);
